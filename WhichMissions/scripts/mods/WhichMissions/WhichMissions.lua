9fb5aae414e60131124325bb22790707c78452fc
deluxghost <deluxghost@users.noreply.github.com>
2023-03-06 22:28:55 +0800

    use built-in localization keys

--- WhichMissions/scripts/mods/WhichMissions/WhichMissions.lua
+++ WhichMissions/scripts/mods/WhichMissions/WhichMissions.lua
@@ -1,4 +1,5 @@
 local mod = get_mod("WhichMissions")
+local DangerSettings = require("scripts/settings/difficulty/danger_settings")
 
 local num_objective_types = 7
 local num_difficulties = 5
@@ -6,15 +7,17 @@ local num_difficulties_class = 3
 
 local function _convert_class(s)
 	if s == "zealot" or s == "preacher" then
-		return "zealot_2"
+		return "zealot_2", "loc_class_zealot_title"
 	elseif s == "psyker" or s == "psykinetic" then
-		return "psyker_2"
+		return "psyker_2", "loc_class_psyker_title"
 	elseif s == "veteran" or s == "sharpshooter" then
-		return "veteran_2"
+		return "veteran_2", "loc_class_veteran_title"
 	elseif s == "ogryn" or s == "skullbreaker" then
-		return "ogryn_2"
+		return "ogryn_2", "loc_class_ogryn_title"
 	end
-	return Managers.player:local_player(1)._profile.specialization
+
+	local profile = Managers.player:local_player(1)._profile
+	return profile.specialization, profile.archetype.archetype_title
 end
 
 local function _convert_account_difficulty(s)
@@ -46,6 +49,18 @@ local function _convert_class_difficulty(s)
 		return 1
 	else
 		return -1
+	end
+end
+
+local function _localize_class_difficulty(s)
+	if s == 3 then
+		return Localize(DangerSettings.by_index[4].display_name)
+	elseif s == 2 then
+		return Localize(DangerSettings.by_index[3].display_name)
+	elseif s == 1 then
+		return mod:localize("difficulty_class_any")
+	else
+		return ""
 	end
 end
 
@@ -87,7 +102,7 @@ local function _get_objective_readout(needed_objectives)
 			if readout ~= "" then
 				readout = readout .. ", "
 			end
-			readout = readout .. mod:localize("mission_type_" .. o)
+			readout = readout .. Localize(string.format("loc_mission_type_%02d_name", o))
 		end
 	end
 	return readout, num_objs
@@ -101,12 +116,12 @@ end
 
 local function _get_class_needs(difficulty_filter, class_filter)
 	Managers.data_service.account:pull_achievement_data():next(function (achievements_data)
-		class_filter = _convert_class(class_filter)
+		local class_filter, class_title = _convert_class(class_filter)
 		difficulty_filter = _convert_class_difficulty(difficulty_filter)
 
 		local readout, num_objs, dfcl = _get_needed_readout("_mission_" .. class_filter, difficulty_filter, num_difficulties_class, achievements_data)
-		local dfcl_localized = mod:localize("difficulty_class_" .. dfcl)
-		local class_localized = mod:localize("class_" .. class_filter)
+		local dfcl_localized = _localize_class_difficulty(dfcl)
+		local class_localized = Localize(class_title)
 		if num_objs == 0 then
 			mod:echo(mod:localize("class_finished", class_localized, dfcl_localized))
 		else
@@ -123,7 +138,7 @@ local function _get_account_needs(difficulty_filter)
 		difficulty_filter = _convert_account_difficulty(difficulty_filter)
 		
 		local readout, num_objs, dfcl = _get_needed_readout("_mission_difficulty", difficulty_filter, num_difficulties, achievements_data)
-		local dfcl_localized = mod:localize("difficulty_account_" .. dfcl)
+		local dfcl_localized = Localize(DangerSettings.by_index[dfcl].display_name)
 		if num_objs == 0 then
 			mod:echo(mod:localize("account_finished", dfcl_localized))
 		else
--- WhichMissions/scripts/mods/WhichMissions/WhichMissions_localization.lua
+++ WhichMissions/scripts/mods/WhichMissions/WhichMissions_localization.lua
@@ -9,78 +9,24 @@ return {
 		en = "Explains how to use WhichMissions",
 	},
 	cmd_desc_class = {
-		en = "WhichMissions you need for a difficulty & class"
+		en = "WhichMissions you need for a difficulty & class",
 	},
 	cmd_desc_account = {
-		en = "WhichMissions you need for a difficulty"
+		en = "WhichMissions you need for a difficulty",
 	},
 	account_needs = {
-		en = "Your account needs %s more on %s:\n%s"
+		en = "Your account needs %s more on %s:\n%s",
 	},
 	account_finished = {
-		en = "Congratulations! You have finished the account-wide penance for %s."
+		en = "Congratulations! You have finished the account-wide penance for %s.",
 	},
 	class_needs = {
-		en = "Your %s needs %s more on %s:\n%s"
+		en = "Your %s needs %s more on %s:\n%s",
 	},
 	class_finished = {
-		en = "Congratulations! Your %s has finished the penance for %s."
+		en = "Congratulations! Your %s has finished the penance for %s.",
 	},
-	class_ogryn_2 = {
-		en = "Ogryn"
-	},
-	class_zealot_2 = {
-		en = "Zealot"
-	},
-	class_psyker_2 = {
-		en = "Psyker"
-	},
-	class_veteran_2 = {
-		en = "Veteran"
-	},
-	mission_type_1 = {
-		en = "Raid"
-	},
-	mission_type_2 = {
-		en = "Assassination"
-	},
-	mission_type_3 = {
-		en = "Investigation"
-	},
-	mission_type_4 = {
-		en = "Disruption"
-	},
-	mission_type_5 = {
-		en = "Strike"
-	},
-	mission_type_6 = {
-		en = "Espionage"
-	},
-	mission_type_7 = {
-		en = "Repair"
-	},
-	difficulty_class_1 = {
-		en = "any difficulty"
-	},
-	difficulty_class_2 = {
-		en = "Malice"
-	},
-	difficulty_class_3 = {
-		en = "Heresy"
-	},
-	difficulty_account_1 = {
-		en = "Sedition"
-	},
-	difficulty_account_2 = {
-		en = "Uprising"
-	},
-	difficulty_account_3 = {
-		en = "Malice"
-	},
-	difficulty_account_4 = {
-		en = "Heresy"
-	},
-	difficulty_account_5 = {
-		en = "Damnation"
+	difficulty_class_any = {
+		en = "any difficulty",
 	},
 }
