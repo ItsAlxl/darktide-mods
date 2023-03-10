20169de166adb0b88857aee077f385bd2bd10cea
deluxghost <deluxghost@users.noreply.github.com>
2023-03-11 07:17:27 +0800

    update zh-cn localization

--- PickForMe/scripts/mods/PickForMe/PickForMe_localization.lua
+++ PickForMe/scripts/mods/PickForMe/PickForMe_localization.lua
@@ -1,32 +1,42 @@
 return {
 	mod_description = {
 		en = "Easily randomize your current loadout. Try '/pickforme help' in the chat.",
+		["zh-cn"] = "快速随机选择配装。试着在聊天框输入 /pickforme help",
 	},
 	cmd_desc = {
 		en = "Randomize your current loadout",
+		["zh-cn"] = "随机选择当前配装",
 	},
 	cmd_help = {
 		en = "/pickforme [args...]\nArgs can include 'weapons', 'primary', 'secondary', 'curios', or 'all'. For example, '/pickforme secondary curios' will randomize your secondary weapon and curios. '/pickforme' without arguments is equivalent to the Quick Randomize configured in the mod settings.",
+		["zh-cn"] = "/pickforme [参数...]\n参数可以是 'weapons', 'primary', 'secondary', 'curios' 或者 'all'。例如，'/pickforme secondary curios' 会随机选择副武器和珍品。不带任何参数的 '/pickforme' 命令效果等同于模组选项中设置的快速随机。",
 	},
 	only_use_in_hub = {
 		en = "You can't use PickForMe during a mission",
+		["zh-cn"] = "你不能在任务中随机配装",
 	},
 	catch_error = {
 		en = "Loadout randomization failed",
+		["zh-cn"] = "配装随机失败",
 	},
 	quick_randomize = {
 		en = "Quick Randomize",
+		["zh-cn"] = "快速随机",
 	},
 	quick_randomize_keybind = {
 		en = "Keybind",
+		["zh-cn"] = "快捷键",
 	},
 	random_primary = {
 		en = "Randomizes primary",
+		["zh-cn"] = "随机主武器",
 	},
 	random_secondary = {
 		en = "Randomizes secondary",
+		["zh-cn"] = "随机副武器",
 	},
 	random_curios = {
 		en = "Randomizes curios",
+		["zh-cn"] = "随机珍品",
 	},
 }
