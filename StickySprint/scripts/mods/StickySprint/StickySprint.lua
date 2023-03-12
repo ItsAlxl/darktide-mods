fc455591c4ab9776c0bc67be61b803fd96283213
alxl <alex.cavasso@gmail.com>
2023-03-12 01:01:56 -0600

    fix StickySprint

--- StickySprint/scripts/mods/StickySprint/StickySprint.lua
+++ StickySprint/scripts/mods/StickySprint/StickySprint.lua
@@ -1,11 +1,23 @@
 local mod = get_mod("StickySprint")
 
+local keep_sprinting = false
+
 mod:hook_require("scripts/extension_systems/character_state_machine/character_states/utilities/sprint", function(instance)
-    mod:hook(instance, "sprint_input", function(func, input_source, is_sprinting, sprint_requires_press_to_interrupt)
-        local hold_to_sprint = input_source:get("hold_to_sprint")
-        if not hold_to_sprint and is_sprinting then
-            return true
+    mod:hook_safe(instance, "sprint_input", function(input_source, is_sprinting, sprint_requires_press_to_interrupt)
+        if keep_sprinting and not is_sprinting then
+            keep_sprinting = false
         end
-        return func(input_source, is_sprinting, sprint_requires_press_to_interrupt)
     end)
+end)
+
+mod:hook("InputService", "get", function(func, self, action_name)
+    local val = func(self, action_name)
+    if action_name == "sprint" then
+        if keep_sprinting then
+            val = false
+        else
+            keep_sprinting = true
+        end
+    end
+    return val
 end)
