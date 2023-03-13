818aeff812aae3015113537a1dce8e5888e15715
deluxghost <deluxghost@users.noreply.github.com>
2023-03-13 16:10:45 +0800

    update zh-cn

--- ReorderChars/scripts/mods/ReorderChars/ReorderChars_localization.lua
+++ ReorderChars/scripts/mods/ReorderChars/ReorderChars_localization.lua
@@ -1,11 +1,14 @@
 return {
 	mod_description = {
 		en = "Reorder your characters on the character select screen.",
+		["zh-cn"] = "为特工选择界面的角色重新排序。",
 	},
 	move_up = {
 		en = "Move Up",
+		["zh-cn"] = "向上移动",
 	},
 	move_down = {
 		en = "Move Down",
+		["zh-cn"] = "向下移动",
 	},
 }
