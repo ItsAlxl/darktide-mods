6ec75e581180705fcf208fb46a07e76fed128add
alxl <alex.cavasso@gmail.com>
2023-03-21 02:36:27 -0500

    FullAuto v1.1
    Simplifies the logic a lot, because some weapons weren't behaving correctly. The chain time is not actually the optimal time to re-click.

--- FullAuto/scripts/mods/FullAuto/FullAuto.lua
+++ FullAuto/scripts/mods/FullAuto/FullAuto.lua
@@ -1,64 +1,41 @@
 local mod = get_mod("FullAuto")
 
-local NORMAL_ACTIONS = { "action_shoot_hip", "action_shoot", "rapid_left" }
-local NORMAL_CHAINS = { "shoot_pressed", "shoot", "shoot_charge" }
+local NORMAL_ACTIONS = { "action_shoot_hip", "action_shoot", "rapid_left", "action_shoot_flame" }
 local AIMED_ACTIONS = { "action_shoot_zoomed" }
-local AIMED_CHAINS = { "zoom_shoot" }
 
 local FULLAUTO_FIREMODE = "full_auto"
+local FIRE_DELAY = 0.1
 
 local track_autofire = false
+local autofire_normal = false
+local autofire_aim = false
 
-local autofire_normal_delay = -1
-local autofire_aim_delay = -1
-
+local autofire_current = false
 local is_firing = false
-local test_delay = -1
 local last_autofire = -1
 
 local function _disable_autofire()
     track_autofire = false
+    autofire_normal = false
+    autofire_aim = false
 
-    autofire_normal_delay = -1
-    autofire_aim_delay = -1
-
+    autofire_current = false
     is_firing = false
-    test_delay = -1
     last_autofire = -1
 end
 
-local function _get_action(template, primary)
+local function _check_action(template, primary)
     local actions = NORMAL_ACTIONS
     if not primary then
         actions = AIMED_ACTIONS
     end
 
     for _, a in pairs(actions) do
-        local act = template.actions[a]
-        if act then
-            return act
+        if template.actions[a] then
+            return true
         end
     end
-    return nil
-end
-
-local function _get_chain_time(template, primary)
-    local act = _get_action(template, primary)
-    if act then
-        local chain_actions = NORMAL_CHAINS
-        if not primary then
-            chain_actions = AIMED_CHAINS
-        end
-
-        for _, ca in pairs(chain_actions) do
-            local chain = act.allowed_chain_actions[ca]
-            if chain then
-                return chain.chain_time
-            end
-        end
-    end
-
-    return -1
+    return false
 end
 
 local function _check_firemode(fm)
@@ -74,22 +51,15 @@ local function _apply_weapon_template(template)
         return
     end
 
-    --[[
-    for t,_ in pairs(template.actions) do
-        mod:echo(t)
-    end
-    mod:echo("%s, %s", template.displayed_attacks.primary.fire_mode, template.displayed_attacks.secondary.fire_mode)
-    --]]
     if _check_firemode(template.displayed_attacks.primary.fire_mode) then
-        autofire_normal_delay = _get_chain_time(template, true)
+        autofire_normal = _check_action(template, true)
     end
     if _check_firemode(template.displayed_attacks.secondary.fire_mode) then
-        autofire_aim_delay = _get_chain_time(template, false)
+        autofire_aim = _check_action(template, false)
     end
 
-    --mod:echo("normal: %s | aimed: %s", autofire_normal_delay >= 0, autofire_aim_delay >= 0)
-    track_autofire = autofire_normal_delay >= 0 or autofire_aim_delay >= 0
-    test_delay = autofire_normal_delay
+    track_autofire = autofire_normal or autofire_aim
+    autofire_current = autofire_normal
 end
 
 mod:hook_safe("PlayerUnitWeaponExtension", "on_slot_wielded", function(self, slot_name, ...)
@@ -113,30 +83,20 @@ mod:hook("InputService", "get", function(func, self, action_name)
             end
 
             if action_name == "action_two_pressed" then
-                if autofire_aim_delay >= 0 then
-                    test_delay = autofire_aim_delay
-                else
-                    test_delay = -1
-                end
+                autofire_current = autofire_aim
             end
             if action_name == "action_two_release" then
-                if autofire_normal_delay >= 0 then
-                    test_delay = autofire_normal_delay
-                else
-                    test_delay = -1
-                end
+                autofire_current = autofire_normal
             end
         end
 
-        if is_firing and action_name == "action_one_pressed" then
-            if test_delay >= 0 then
-                local this_t = Managers.time and Managers.time:time("main")
-                if last_autofire < 0 or this_t - last_autofire >= test_delay then
-                    last_autofire = this_t
-                    return true
-                end
-                return false
+        if is_firing and autofire_current and action_name == "action_one_pressed" then
+            local this_t = Managers.time and Managers.time:time("main")
+            if last_autofire < 0 or this_t - last_autofire >= FIRE_DELAY then
+                last_autofire = this_t
+                return true
             end
+            return false
         end
     end
 
