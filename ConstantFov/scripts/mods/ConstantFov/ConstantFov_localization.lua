f60a8a775898a792c5bf70c9f7176e74d97bb660
xsSplater <xssplater@gmail.com>
2023-03-28 21:42:13 +0300

    Update ConstantFov_localization.lua
    Russian localization.

--- ConstantFov/scripts/mods/ConstantFov/ConstantFov_localization.lua
+++ ConstantFov/scripts/mods/ConstantFov/ConstantFov_localization.lua
@@ -2,41 +2,54 @@ return {
 	mod_description = {
 		en = "Tweak FOV-changing effects.",
 		["zh-cn"] = "禁用所有视野变化特效。",
+		ru = "Настройте эффекты изменяющие поле зрения(FOV).",
 	},
 	group_toggles = {
 		en = "Allowed FOV Changes",
+		ru = "Разрешенные изменения поля зрения",
 	},
 	allow_aim = {
 		en = "Aiming",
+		ru = "Прицеливание",
 	},
 	allow_lunge = {
 		en = "Ogryn/Zealot Charge",
+		ru = "Рывок Огрина/Изувера",
 	},
 	allow_sprint = {
 		en = "Sprinting",
+		ru = "Спринт",
 	},
 	allow_vetult = {
 		en = "Veteran Volley Fire",
+		ru = "Залповый огонь Ветерана",
 	},
 	group_tweaks = {
 		en = "FOV Change Tweaks",
+		ru = "Настройки изменения поля зрения",
 	},
 	change_multiplier = {
 		en = "FOV Change Multiplier",
+		ru = "Множ. изменения поля зрения",
 	},
 	change_multiplier_description = {
 		en = "Increase or decrease how drastic FOV changes are. A value of 0.5 makes all FOV changes half as strong; a value of 2.0 makes all FOV changes twice as strong.",
+		ru = "Увеличьте или уменьшите силу изменения поле зрения. Значение 0,5 делает все изменения поля зрения в два раза слабее; значение 2.0 делает все изменения поля зрения в два раза сильнее.",
 	},
 	limit_lower = {
 		en = "Minimum FOV Change",
+		ru = "Мин. изменение поля зрения",
 	},
 	limit_upper = {
 		en = "Maximum FOV Change",
+		ru = "Макс. изменение поля зрения",
 	},
 	apply_baseline = {
 		en = "Apply Baseline Multiplier",
+		ru = "Применить базовый множитель",
 	},
 	apply_baseline_description = {
 		en = "Darktide normally applies a baseline FOV multiplier. Turn it off if you want a 'rawer' value.",
+		ru = "Darktide обычно применяет базовый множитель поля зрения. Выключите его, если вы хотите получить более \"грубое\" значение.",
 	},
 }
