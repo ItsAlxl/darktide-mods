3b16ff79e9a05a5ccea28ce7ce4f62794bdce2c8
xsSplater <xssplater@gmail.com>
2023-03-28 17:19:44 +0300

    Update ReorderChars_localization.lua
    Russian localization.

--- ReorderChars/scripts/mods/ReorderChars/ReorderChars_localization.lua
+++ ReorderChars/scripts/mods/ReorderChars/ReorderChars_localization.lua
@@ -2,13 +2,16 @@ return {
 	mod_description = {
 		en = "Reorder your characters on the character select screen.",
 		["zh-cn"] = "为特工选择界面的角色重新排序。",
+		ru = "Измените порядок расположения своих персонажей на экране выбора персонажей.",
 	},
 	move_up = {
 		en = "Move Up",
 		["zh-cn"] = "向上移动",
+		ru = "Вверх",
 	},
 	move_down = {
 		en = "Move Down",
 		["zh-cn"] = "向下移动",
+		ru = "Вниз",
 	},
 }
