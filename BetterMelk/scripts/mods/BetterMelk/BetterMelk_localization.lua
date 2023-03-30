8fbdd88cc424b21837a6df1e30c2a3bd8f15befb
xsSplater <xssplater@gmail.com>
2023-03-30 13:20:31 +0300

    Update BetterMelk_localization.lua
    Russian localization.

--- BetterMelk/scripts/mods/BetterMelk/BetterMelk_localization.lua
+++ BetterMelk/scripts/mods/BetterMelk/BetterMelk_localization.lua
@@ -2,37 +2,46 @@ return {
 	mod_description = {
 		en = "Automatically accept new contracts and collect the weekly completion bonus.",
 		["zh-cn"] = "自动接受新协议并收集每周协议额外奖励。",
+		ru = "Автоматически принимайте новые контракты и получайте еженедельный бонус за выполнение.",
 	},
 	notify_new = {
 		en = "New Contracts Message",
 		["zh-cn"] = "新协议消息",
+		ru = "Сообщение о новых контрактах",
 	},
 	msg_new = {
 		en = "You have new contracts with Sire Melk.",
 		["zh-cn"] = "你有梅尔克大人的新协议。",
+		ru = "Вы получили новые контракты от сира Мелка.",
 	},
 	notify_done = {
 		en = "All Finished Message",
 		["zh-cn"] = "全部完成消息",
+		ru = "Сообщение о готовности",
 	},
 	msg_done = {
 		en = "Congratulations! You have finished your weekly contracts.",
 		["zh-cn"] = "恭喜！你完成了每周协议。",
+		ru = "Поздравляем! Вы закончили свои еженедельные контракты.",
 	},
 	notif_mode = {
 		en = "Notification Mode",
 		["zh-cn"] = "通知模式",
+		ru = "Режим уведомлений",
 	},
 	channel_chat = {
 		en = "Chat",
 		["zh-cn"] = "聊天",
+		ru = "Чат",
 	},
 	channel_notifs = {
 		en = "Notification",
 		["zh-cn"] = "通知",
+		ru = "Уведомление",
 	},
 	channel_both = {
 		en = "Chat + Notification",
 		["zh-cn"] = "聊天和通知",
+		ru = "Чат + Уведомление",
 	},
 }
