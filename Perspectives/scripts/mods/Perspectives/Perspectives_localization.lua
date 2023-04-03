6413c594ffebb42e87ca1a38da4bcf62b05d616d
deluxghost <deluxghost@users.noreply.github.com>
2023-04-03 11:14:58 +0800

    update zh-cn

--- Perspectives/scripts/mods/Perspectives/Perspectives_localization.lua
+++ Perspectives/scripts/mods/Perspectives/Perspectives_localization.lua
@@ -1,44 +1,58 @@
 return {
 	mod_description = {
 		en = "Switch between first and third person perspectives.",
+		["zh-cn"] = "在第一人称和第三人称视角之间切换。",
 	},
 	toggle_perspective_keybind = {
 		en = "Toggle Perspective",
+		["zh-cn"] = "切换视角",
 	},
 	cycle_shoulder = {
 		en = "Cycle Aiming Shoulders",
+		["zh-cn"] = "切换越肩",
 	},
 	aim_mode = {
 		en = "Aiming Behavior",
+		["zh-cn"] = "瞄准行为",
 	},
 	aim_cycle = {
 		en = "Shoulders",
+		["zh-cn"] = "越肩",
 	},
 	aim_center = {
 		en = "Centered 3rd Person",
+		["zh-cn"] = "中心对齐第三人称",
 	},
 	aim_1p = {
 		en = "Switch to 1st Person",
+		["zh-cn"] = "切换到第一人称",
 	},
 	cycle_includes_center = {
 		en = "Include 'Centered 3rd Person' in Shoulder Cycle",
+		["zh-cn"] = "在越肩切换中包含“中心对齐第三人称”",
 	},
 	perspective_transition_time = {
 		en = "Perspective Transition Time",
+		["zh-cn"] = "视角切换时间",
 	},
 	default_perspective_mode = {
 		en = "Initial Perspective",
+		["zh-cn"] = "初始视角",
 	},
 	defper_normal = {
 		en = "Default",
+		["zh-cn"] = "默认",
 	},
 	defper_swapped = {
 		en = "Opposite of Default",
+		["zh-cn"] = "反转默认",
 	},
 	defper_always_first = {
 		en = "1st Person",
+		["zh-cn"] = "第一人称",
 	},
 	defper_always_third = {
 		en = "3rd Person",
+		["zh-cn"] = "第三人称",
 	},
 }
