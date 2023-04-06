a4a9288f67c4f54956ed577083da52e8cac9615e
xsSplater <xssplater@gmail.com>
2023-04-07 00:09:23 +0300

    Update WhichMissions_localization.lua

--- WhichMissions/scripts/mods/WhichMissions/WhichMissions_localization.lua
+++ WhichMissions/scripts/mods/WhichMissions/WhichMissions_localization.lua
@@ -1,4 +1,8 @@
 return {
+	mod_name = {
+		en = "WhichMissions",
+		ru = "Какие Миссии",
+	},
 	mod_description = {
 		en = "Provides some commands to help you figure out which missions you're missing for penances. Try /wm_help in the chat.",
 		["zh-cn"] = "提供一些命令，查看你的任务苦修还缺少哪些任务类型。在聊天中输入 /wm_help 获取帮助。",
@@ -7,7 +11,7 @@ return {
 	mod_help = {
 		en = "Note: all command arguments must be provided in lowercase. Alternatively, difficulty can be provided as a number (1-5). To skip an optional argument, use * (eg /wm_class * psyker).\n\n/wm_account [difficulty]\nLists which mission types you need for a given difficulty for the account-wide penances. If no difficulty is provided, it defaults to the lowest difficulty you haven't finished.\n\n/wm_class [difficulty] [class]\nLists which mission types you need for a given difficulty and class. If no class is provided, it defaults to your current class. If no difficulty is provided, it defaults to the lowest difficulty you haven't finished.",
 		["zh-cn"] = "注意：所有命令参数必须为小写。难度参数也可以为数字（1-5）。要跳过可选参数，使用星号 * 代替\n（例如 /wm_class * psyker）。\n可用难度：sedition, uprising, malice, heresy, damnation\n可用职业：veteran, zealot, ogryn, psyker\n\n/wm_account [难度]\n列出指定难度的全职业任务苦修还缺少哪些任务类型。如果未提供难度，默认为当前尚未完成的最低难度。\n\n/wm_class [难度] [职业]\n列出指定难度的分职业任务苦修还缺少哪些任务类型。如果未提供职业，默认为当前职业。如果未提供难度，默认为当前尚未完成的最低难度。",
-		ru = "Примечание: все аргументы команд должны быть написаны в нижнем регистре. В качестве альтернативы, сложность может быть указана в виде числа (1-5). Чтобы пропустить необязательный аргумент, используйте * (например, /wm_class * psyker).\n\n/wm_account [difficulty]\nПоказывает, какие типы миссий вам нужны на данной сложности для Искуплений всей учетной записи. Если сложность не указана, по умолчанию используется самая низкая сложность, которую вы еще не прошли.\n\n/wm_class [difficulty] [class]\nПоказывает типы миссий, которые вам нужны для данной сложности и класса. Если класс не указан, по умолчанию используется текущий класс. Если сложность не указана, по умолчанию используется самая низкая сложность, которую вы еще не прошли.",
+		ru = "Примечание: все аргументы команд должны быть написаны в нижнем регистре. В качестве альтернативы, сложность может быть указана в виде числа (1-5). Чтобы пропустить необязательный аргумент, используйте * (например, /wm_class * psyker).\n\n/wm_account [difficulty]\nПоказывает, какие типы миссий вам нужны на данной сложности для Искуплений всей учётной записи. Если сложность не указана, по умолчанию используется самая низкая сложность, которую вы ещё не прошли.\n\n/wm_class [difficulty] [class]\nПоказывает типы миссий, которые вам нужны для данной сложности и класса. Если класс не указан, по умолчанию используется текущий класс. Если сложность не указана, по умолчанию используется самая низкая сложность, которую вы еще не прошли.",
 	},
 	cmd_desc_help = {
 		en = "Explains how to use WhichMissions",
@@ -37,7 +41,7 @@ return {
 	account_finished = {
 		en = "Congratulations! You have finished the account-wide penance for %s.",
 		["zh-cn"] = "恭喜！你已经完成了%s的全职业任务苦修。",
-		ru = "Поздравляем! Вы завершили Искупления для всей учетной записи на сложности %s.",
+		ru = "Поздравляем! Вы завершили Искупления для всей учётной записи на сложности %s.",
 	},
 	class_needs = {
 		en = "Your %s needs %s more on %s:\n%s",
