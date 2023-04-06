393165b5180ccbe1e25606fec3c1c66608ac1260
xsSplater <xssplater@gmail.com>
2023-04-06 10:33:30 +0300

    ConstantFov.  Added the possibility of localization of the name.

--- ConstantFov/scripts/mods/ConstantFov/ConstantFov_data.lua
+++ ConstantFov/scripts/mods/ConstantFov/ConstantFov_data.lua
@@ -1,7 +1,7 @@
 local mod = get_mod("ConstantFov")
 
 return {
-	name = "ConstantFov",
+	name = mod:localize("mod_name"),
 	description = mod:localize("mod_description"),
 	is_togglable = true,
 	options = {
