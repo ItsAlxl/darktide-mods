453bd50279648e1728e23e6684b79264d8229890
deluxghost <deluxghost@users.noreply.github.com>
2023-05-06 11:21:14 +0800

    update zh-cn

--- Walk/scripts/mods/Walk/Walk_localization.lua
+++ Walk/scripts/mods/Walk/Walk_localization.lua
@@ -1,17 +1,22 @@
 return {
 	mod_description = {
 		en = "Walk slowly at the push of a button",
+		["zh-cn"] = "按下按键时慢走",
 	},
 	walk_key_toggle = {
 		en = "Walk (Toggle)",
+		["zh-cn"] = "慢走（开关）",
 	},
 	walk_key_held = {
 		en = "Walk (Held)",
+		["zh-cn"] = "慢走（按住）",
 	},
 	walk_speed = {
 		en = "Walk Speed Multiplier",
+		["zh-cn"] = "行走速度倍数",
 	},
 	sprint_cancels = {
 		en = "Sprinting cancels walk",
+		["zh-cn"] = "疾跑时停止慢走",
 	},
 }
