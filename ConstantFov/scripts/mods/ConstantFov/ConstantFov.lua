6659fcdde8a10528e2d0442c1a4f98dc0b887cae
alxl <alex.cavasso@gmail.com>
2023-07-06 05:00:12 -0500

    ConstantFov 1.5
    fix for DT Patch #11 (hopefully future-proof)

--- ConstantFov/scripts/mods/ConstantFov/ConstantFov.lua
+++ ConstantFov/scripts/mods/ConstantFov/ConstantFov.lua
@@ -1,6 +1,14 @@
 local mod = get_mod("ConstantFov")
+
 local DEFAULT_BASE_MULT = 1.1344640137963142
 local VeteranAbilities = require("scripts/settings/ability/player_abilities/veteran_abilities")
+
+local vet_ult_buffs = {}
+for _, ab in pairs(VeteranAbilities) do
+    if ab.ability_template_tweak_data and ab.ability_template_tweak_data.buff_to_add and ab.ability_type and ab.ability_type == "combat_ability" then
+        table.insert(vet_ult_buffs, ab.ability_template_tweak_data.buff_to_add)
+    end
+end
 
 local fov_data = {
     base = mod:get("apply_baseline") and DEFAULT_BASE_MULT or 1.0,
@@ -33,7 +41,7 @@ mod.on_setting_changed = function(id)
     end
 end
 
-mod:hook(CLASS.CameraManager, "_update_camera_properties", function(func, self, camera, shadow_cull_camera, current_node, camera_data, viewport_name)
+mod:hook(CLASS.CameraManager, "_update_camera_properties", function(func, self, camera, shadow_cull_camera, current_node, camera_data, ...)
     if camera_data.vertical_fov then
         local check = allow_nodes[current_node._name]
         if check == nil or check then
@@ -42,14 +50,14 @@ mod:hook(CLASS.CameraManager, "_update_camera_properties", function(func, self, 
             camera_data.vertical_fov = fov_data.base
         end
     end
-    func(self, camera, shadow_cull_camera, current_node, camera_data, viewport_name)
+    func(self, camera, shadow_cull_camera, current_node, camera_data, ...)
 end)
 
-mod:hook(CLASS.Buff, "update_stat_buffs", function(func, self, current_stat_buffs, t)
-    func(self, current_stat_buffs, t)
+mod:hook(CLASS.Buff, "update_stat_buffs", function(func, self, current_stat_buffs, ...)
+    func(self, current_stat_buffs, ...)
     if not fov_data.allow_vetult then
         local fov_multiplier = current_stat_buffs.fov_multiplier
-        if fov_multiplier and VeteranAbilities[self:template_name()] then
+        if fov_multiplier and table.contains(vet_ult_buffs, self:template_name()) then
             current_stat_buffs.fov_multiplier = 1
         end
     end
@@ -62,12 +70,14 @@ mod:hook_require("scripts/settings/camera/camera_transition_templates", function
         end
         return 1.0
     end)
+
     mod:hook(CameraTransitionTemplates.from_lunge.vertical_fov, "transition_func", function(func, t)
         if allow_nodes.lunge then
             return func(t)
         end
         return 1.0
     end)
+
     mod:hook(CameraTransitionTemplates.zoom.vertical_fov, "transition_func", function(func, t)
         if allow_nodes.aim_down_sight then
             return func(t)
