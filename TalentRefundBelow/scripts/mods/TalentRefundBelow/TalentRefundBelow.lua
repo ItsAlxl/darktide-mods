86b41b4939e2f31910d4cbccb2b0feeec49ec57b
alxl <alex.cavasso@gmail.com>
2023-10-07 02:44:15 -0500

    TalentRefundBelow 1.1
    Fix inconsistent hooking

--- TalentRefundBelow/scripts/mods/TalentRefundBelow/TalentRefundBelow.lua
+++ TalentRefundBelow/scripts/mods/TalentRefundBelow/TalentRefundBelow.lua
@@ -1,10 +1,11 @@
 local mod = get_mod("TalentRefundBelow")
 
-mod:hook(CLASS.NodeBuilderViewBase, "_on_node_widget_right_pressed", function(func, self, widget)
+mod:hook(CLASS.TalentBuilderView, "_on_node_widget_right_pressed", function(func, self, widget)
     local node = widget.content.node_data
     local widgets_by_name = self._widgets_by_name
 
-    for _, child_name in ipairs(node.children) do
+    local children = node.children
+    for _, child_name in ipairs(children) do
         local child_node = self:_node_by_name(child_name)
         if child_node and self:_is_node_dependent_on_parent(child_node, node) then
             self:_on_node_widget_right_pressed(widgets_by_name[child_node.widget_name])
