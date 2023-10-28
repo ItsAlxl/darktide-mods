83e301e97df2dfbd1a21a42c924492aa19010c8d
deluxghost <deluxghost@users.noreply.github.com>
2023-10-28 13:26:22 +0800

    update zh-cn

--- IgnorePsykerGrenades/scripts/mods/IgnorePsykerGrenades/IgnorePsykerGrenades_localization.lua
+++ IgnorePsykerGrenades/scripts/mods/IgnorePsykerGrenades/IgnorePsykerGrenades_localization.lua
@@ -2,10 +2,12 @@ return {
 	mod_name = {
 		en = "Ignore Psyker Grenades",
 		ru = "Игнорирование псайкерских гранат",
+		["zh-cn"] = "忽略灵能者手雷",
 	},
 	mod_description = {
 		en = "Prevent Psyker blitzes from changing their grenade color in your HUD.",
 		ru = "Ignore Psyker Grenades - Предотвращает изменение цвета гранат в вашем интерфейсе при использовании псайкерских талантов.",
+		["zh-cn"] = "禁止灵能者闪击技能修改 HUD 上的手雷颜色",
 	},
 	psyker_throwing_knives = {
 		en = Localize("loc_ability_psyker_blitz_throwing_knives"),
