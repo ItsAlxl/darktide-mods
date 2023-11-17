0656a5692f430b7769a4053e3e75d74540a6d3fe
deluxghost <deluxghost@users.noreply.github.com>
2023-11-17 16:19:24 +0800

    update zh-cn

--- BornReady/scripts/mods/BornReady/BornReady_localization.lua
+++ BornReady/scripts/mods/BornReady/BornReady_localization.lua
@@ -1,37 +1,47 @@
 return {
 	mod_name = {
-		en = "BornReady",
+		en = "Born Ready",
 		ru = "Рождён готовым",
 		["zh-cn"] = "自动准备",
 	},
 	mod_description = {
 		en = "Options to auto-accept matchmaking, lobbies, and party invites.",
+		["zh-cn"] = "提供选项自动接受匹配、任务准备和小队邀请。",
 	},
 	leave_party = {
 		en = "Leave Current Party",
+		["zh-cn"] = "离开当前小队",
 	},
 	autoready = {
 		en = "Ready up in lobby",
+		["zh-cn"] = "在任务中准备",
 	},
 	autoskip = {
 		en = "Skip end-of-mission screen",
+		["zh-cn"] = "跳过任务结束界面",
 	},
 	automatch = {
 		en = "Accept mission prompt",
+		["zh-cn"] = "接受任务请求",
 	},
 	autojoin = {
 		en = "Party invites",
+		["zh-cn"] = "小队邀请",
 	},
 	autowelcome = {
 		en = "Requests to join your party",
+		["zh-cn"] = "请求加入你的小队",
 	},
 	none = {
 		en = "Prompt",
+		["zh-cn"] = "提示",
 	},
 	auto_yes = {
 		en = "Automatically Accept",
+		["zh-cn"] = "自动接受",
 	},
 	auto_no = {
 		en = "Automatically Decline",
+		["zh-cn"] = "自动拒绝",
 	},
 }
