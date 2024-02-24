ae560c4cffb000aa047f1c350a6c62a4a8fd2431
alxl <alex.cavasso@gmail.com>
2024-02-24 00:15:11 -0600

    FriendlyNPCs rewrite

--- FriendlyNPCs/scripts/mods/FriendlyNPCs/FriendlyNPCs_data.lua
+++ FriendlyNPCs/scripts/mods/FriendlyNPCs/FriendlyNPCs_data.lua
@@ -1,21 +1,26 @@
 local mod = get_mod("FriendlyNPCs")
 
+local opinion_opts = {
+	{ text = "opinion_likes",    value = "likes_character" },
+	{ text = "opinion_dislikes", value = "dislikes_character" },
+	{ text = "opinion_default",  value = "" },
+}
+
+local widgets = {}
+for breed, _ in pairs(mod.opinions) do
+	table.insert(widgets, {
+		setting_id = breed,
+		type = "dropdown",
+		default_value = "likes_character",
+		options = table.clone(opinion_opts)
+	})
+end
+
 return {
 	name = "FriendlyNPCs",
 	description = mod:localize("mod_description"),
 	is_togglable = true,
 	options = {
-		widgets = {
-			{
-				setting_id = "target_opinion",
-				type = "dropdown",
-				default_value = "likes_character",
-				options = {
-					{ text = "opinion_likes", value = "likes_character" },
-					{ text = "opinion_dislikes", value = "dislikes_character" },
-					{ text = "opinion_default", value = "" },
-				}
-			}
-		}
+		widgets = widgets
 	}
 }
--- FriendlyNPCs/scripts/mods/FriendlyNPCs/FriendlyNPCs_localization.lua
+++ FriendlyNPCs/scripts/mods/FriendlyNPCs/FriendlyNPCs_localization.lua
@@ -1,18 +1,16 @@
-return {
+local mod = get_mod("FriendlyNPCs")
+
+local localization = {
 	mod_description = {
 		en = "Set whether vendors are friendly or mean.",
 		["zh-cn"] = "设置 NPC 是否对你友好。",
-	},
-	target_opinion = {
-		en = "Vendor Demeanor",
-		["zh-cn"] = "NPC 态度",
 	},
 	opinion_likes = {
 		en = "Friendly",
 		["zh-cn"] = "友好",
 	},
 	opinion_dislikes = {
-		en = "Jerks",
+		en = "Mean",
 		["zh-cn"] = "恶劣",
 	},
 	opinion_default = {
@@ -20,3 +18,14 @@ return {
 		["zh-cn"] = "默认",
 	},
 }
+
+local DialogueBreedSettings = require("scripts/settings/dialogue/dialogue_breed_settings")
+mod.opinions = {}
+for breed, data in pairs(DialogueBreedSettings) do
+	if data.opinion_settings then
+		mod.opinions[breed] = data.opinion_settings
+		localization[breed] = { en = Localize("loc_npc_short_name_" .. breed .. "_a") }
+	end
+end
+
+return localization
