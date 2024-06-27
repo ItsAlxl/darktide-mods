65c3ebc8f4b06468b54002e2404e7f72beceb49e
alxl <alex.cavasso@gmail.com>
2024-06-26 20:24:29 -0500

    BetterMelk 1.9
    Relocated the character screen counter, removed all the now-vanilla behavior

--- BetterMelk/scripts/mods/BetterMelk/BetterMelk_data.lua
+++ BetterMelk/scripts/mods/BetterMelk/BetterMelk_data.lua
@@ -7,48 +7,17 @@ return {
 	options = {
 		widgets = {
 			{
-				setting_id  = "options_charscreen",
-				type        = "group",
-				sub_widgets = {
-					{
-						setting_id    = "show_contracts",
-						type          = "checkbox",
-						default_value = true,
-					},
-					{
-						setting_id    = "contracts_x",
-						type          = "numeric",
-						default_value = 0,
-						range         = { -10, 250 },
-					},
-				}
+				setting_id    = "offset_x",
+				type          = "numeric",
+				default_value = 0,
+				range         = { -20, 350 },
 			},
 			{
-				setting_id  = "options_notif",
-				type        = "group",
-				sub_widgets = {
-					{
-						setting_id    = "notify_new",
-						type          = "checkbox",
-						default_value = false,
-					},
-					{
-						setting_id    = "notify_done",
-						type          = "checkbox",
-						default_value = true,
-					},
-					{
-						setting_id    = "notif_mode",
-						type          = "dropdown",
-						default_value = 1,
-						options       = {
-							{ text = "channel_both",   value = 0 },
-							{ text = "channel_chat",   value = 1 },
-							{ text = "channel_notifs", value = 2 },
-						},
-					},
-				}
-			}
+				setting_id    = "offset_y",
+				type          = "numeric",
+				default_value = 0,
+				range         = { -10, 80 },
+			},
 		}
 	}
 }
--- BetterMelk/scripts/mods/BetterMelk/BetterMelk_localization.lua
+++ BetterMelk/scripts/mods/BetterMelk/BetterMelk_localization.lua
@@ -1,73 +1,17 @@
 return {
 	mod_name = {
-		en = "BetterMelk",
-		ru = "Лучший Мелк",
+		en = "Character Screen Contracts",
 		["zh-cn"] = "梅尔克协议优化",
+		ru = "Лучший Мелк",
 	},
 	mod_description = {
-		en = "Automatically accept new contracts and collect the weekly completion bonus.",
-		["zh-cn"] = "自动接受新协议并收集每周协议额外奖励。",
-		ru = "Better Melk - Автоматически принимайте новые контракты и получайте еженедельный бонус за их выполнение.",
+		en = "Displays contract progress on character select.",
 	},
-	options_charscreen = {
-		en = "Character Screen Widget",
-		["zh-cn"] = "角色界面显示",
-		ru = "Виджет на экране выбора персонажей",
+	offset_x = {
+		en = "Horizontal Offset",
 	},
-	show_contracts = {
-		en = "Show Contracts in Character Select",
-		["zh-cn"] = "在角色选择界面显示协议",
-		ru = "Показывать контракты на экране выбора персонажей",
-	},
-	contracts_x = {
-		en = "Contracts Offset",
-		["zh-cn"] = "每周协议偏移量",
-		ru = "Смещение контрактов",
-	},
-	options_notif = {
-		en = "Notification",
-		["zh-cn"] = "通知",
-		ru = "Уведомление",
-	},
-	notify_new = {
-		en = "New Contracts Message",
-		["zh-cn"] = "新协议消息",
-		ru = "Сообщение о новых контрактах",
-	},
-	msg_new = {
-		en = "You have new contracts with Sire Melk.",
-		["zh-cn"] = "你有梅尔克大人的新协议。",
-		ru = "Вы получили новые контракты от сира Мелка.",
-	},
-	notify_done = {
-		en = "All Finished Message",
-		["zh-cn"] = "全部完成消息",
-		ru = "Сообщение о готовности",
-	},
-	msg_done = {
-		en = "Congratulations! You have finished your weekly contracts.",
-		["zh-cn"] = "恭喜！你完成了每周协议。",
-		ru = "Поздравляем! Вы закончили свои еженедельные контракты.",
-	},
-	notif_mode = {
-		en = "Notification Mode",
-		["zh-cn"] = "通知模式",
-		ru = "Режим уведомлений",
-	},
-	channel_chat = {
-		en = "Chat",
-		["zh-cn"] = "聊天",
-		ru = "Чат",
-	},
-	channel_notifs = {
-		en = "Notification",
-		["zh-cn"] = "通知",
-		ru = "Уведомление",
-	},
-	channel_both = {
-		en = "Chat + Notification",
-		["zh-cn"] = "聊天和通知",
-		ru = "Чат + Уведомление",
+	offset_y = {
+		en = "Vertical Offset",
 	},
 	msg_error = {
 		en = "Unable to fetch weekly contracts",
