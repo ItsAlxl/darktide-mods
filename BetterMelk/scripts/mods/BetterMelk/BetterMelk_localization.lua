34bebf166431518283a7ea4b8c7ff7c23dd8c8c1
deluxghost <deluxghost@users.noreply.github.com>
2024-07-01 11:21:37 +0800

    update zh-cn translation

--- BetterMelk/scripts/mods/BetterMelk/BetterMelk_localization.lua
+++ BetterMelk/scripts/mods/BetterMelk/BetterMelk_localization.lua
@@ -6,34 +6,42 @@ return {
 	},
 	mod_description = {
 		en = "Displays contract progress on character select.",
+		["zh-cn"] = "在角色选择界面显示每周协议进度。",
 		ru = "Character Screen Contracts - Показывает индикатор контрактов на экране выбора персонажей.",
 	},
 	corner = {
 		en = "Corner",
+		["zh-cn"] = "对齐角",
 		ru = "Угол",
 	},
 	corner_tl = {
 		en = "Top-Left",
+		["zh-cn"] = "左上角",
 		ru = "Сверху слева",
 	},
 	corner_tr = {
 		en = "Top-Right",
+		["zh-cn"] = "右上角",
 		ru = "Сверху справа",
 	},
 	corner_bl = {
 		en = "Bottom-Left",
+		["zh-cn"] = "左下角",
 		ru = "Снизу слева",
 	},
 	corner_br = {
 		en = "Bottom-Right",
+		["zh-cn"] = "右下角",
 		ru = "Снизу справа",
 	},
 	offset_x = {
 		en = "Horizontal Offset",
+		["zh-cn"] = "水平偏移量",
 		ru = "Горизонтальное смещение",
 	},
 	offset_y = {
 		en = "Vertical Offset",
+		["zh-cn"] = "垂直偏移量",
 		ru = "Вертикальное смещение",
 	},
 	msg_error = {
