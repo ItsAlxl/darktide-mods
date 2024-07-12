f001048871c1811c802822bf58701419a1fe814b
alxl <alex.cavasso@gmail.com>
2024-07-12 08:27:06 -0500

    BioPage zh-cn by DingXiang233

--- BioPage/scripts/mods/BioPage/BioPage_localization.lua
+++ BioPage/scripts/mods/BioPage/BioPage_localization.lua
@@ -1,11 +1,14 @@
 return {
 	mod_name = {
 		en = "BioPage",
+		["zh-cn"] = "个人传记",
 	},
 	mod_description = {
 		en = "Adds a Biography page for you and other players",
+		["zh-cn"] = "为您和其他玩家添加一个传记页面，用于说明该玩家选择的出生背景故事",
 	},
 	bio_tab_name = {
 		en = "Biography",
+		["zh-cn"] = "背景故事",
 	},
 }
