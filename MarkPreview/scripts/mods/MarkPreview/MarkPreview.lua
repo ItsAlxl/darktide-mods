f287e4793fb3a50c505efb9b29bd61dd139d1a7d
alxl <alex.cavasso@gmail.com>
2024-09-28 03:45:24 -0500

    MarkPreview 1.1

--- MarkPreview/scripts/mods/MarkPreview/MarkPreview.lua
+++ MarkPreview/scripts/mods/MarkPreview/MarkPreview.lua
@@ -1,16 +1,17 @@
 local mod = get_mod("MarkPreview")
 
 local ViewElementWeaponActionsExtended = require("scripts/ui/view_elements/view_element_weapon_actions/view_element_weapon_actions_extended")
+local WeaponStats = require("scripts/utilities/weapon_stats")
 
 mod:hook_safe(CLASS.InventoryWeaponMarksView, "on_enter", function(self)
-	if not self._weapon_actions_extended then
+	if not self._mkpvw_weapon_actions then
 		local layer = 1
 		local edge_padding = 4
 		local grid_width = 460
 		local grid_height = 1000
 		local actions_grid = self:_add_element(
 			ViewElementWeaponActionsExtended,
-			"mkpvw_weapon_actions_extended",
+			"mkpvw_mkpvw_weapon_actions",
 			layer,
 			{
 				ignore_blur = true,
@@ -42,12 +43,65 @@ mod:hook_safe(CLASS.InventoryWeaponMarksView, "on_enter", function(self)
 		end
 		actions_grid:set_active(true)
 
-		self._weapon_actions_extended = actions_grid
+		actions_grid._mkpvw_extras = true
+		self._mkpvw_weapon_actions = actions_grid
 	end
 end)
 
 mod:hook_safe(CLASS.InventoryWeaponMarksView, "_preview_item", function(self, item)
-	if self._weapon_actions_extended then
-		self._weapon_actions_extended:present_item(item)
+	if self._mkpvw_weapon_actions then
+		self._mkpvw_weapon_actions._mkvpw_item = item
+		self._mkpvw_weapon_actions:present_item(item)
+
+		mod.DBG_wae = self._mkpvw_weapon_actions
+		mod.DBG_wep_stats = WeaponStats:new(item)
 	end
+end)
+
+mod:hook(CLASS.ViewElementWeaponActionsExtended, "present_grid_layout", function(func, self, layout)
+	if self._mkpvw_extras then
+		layout[#layout] = nil
+
+		local weapon_stats = WeaponStats:new(self._mkvpw_item)
+		if (weapon_stats._rate_of_fire and weapon_stats._rate_of_fire > 0.0) then
+			layout[#layout + 1] = {
+				widget_type = "weapon_stat",
+				stat = {
+					type_data = {
+						display_type = "default",
+						display_units = "/s",
+						display_name = "loc_weapon_stats_display_rate_of_fire",
+					},
+					value = weapon_stats._rate_of_fire
+				},
+			}
+		end
+		if (weapon_stats._reload_time and weapon_stats._reload_time > 0.0) then
+			layout[#layout + 1] = {
+				widget_type = "weapon_stat",
+				stat = {
+					type_data = {
+						display_type = "default",
+						display_units = "s",
+						display_name = "loc_basic_reload_input",
+					},
+					value = weapon_stats._reload_time
+				},
+			}
+		end
+		if (weapon_stats._charge_duration and weapon_stats._charge_duration > 0.0) then
+			layout[#layout + 1] = {
+				widget_type = "weapon_stat",
+				stat = {
+					type_data = {
+						display_type = "default",
+						display_units = "s",
+						display_name = "loc_weapon_stats_display_charge_speed",
+					},
+					value = weapon_stats._charge_duration
+				},
+			}
+		end
+	end
+	func(self, layout)
 end)
