ad877bbf33681a39398da18201629c130b2e8dbc
xsSplater - СанСаныч <xssplater@gmail.com>
2024-09-29 03:30:37 +0300

    Update GoToMastery_localization.lua

--- GoToMastery/scripts/mods/GoToMastery/GoToMastery_localization.lua
+++ GoToMastery/scripts/mods/GoToMastery/GoToMastery_localization.lua
@@ -4,10 +4,12 @@ local loc = {
 	mod_name = {
 		en = "GoToMastery",
 		["zh-cn"] = "一键跳转专精",
+		ru = "В Мастерство",
 	},
 	mod_description = {
 		en = "Adds a Mastery button to go to a selected weapon's Mastery menu.",
 		["zh-cn"] = "添加专精按钮，用来前往已选武器的专精菜单。",
+		ru = "Добавляет кнопку «Мастерство» для перехода в меню мастерства выбранного оружия.",
 	},
 }
 
