b8d530445e28da4fdb2e70226ae979558f57014f
xsSplater - СанСаныч <xssplater@gmail.com>
2024-09-29 03:40:30 +0300

    RU MarkPreview_localization.lua (#11)

--- MarkPreview/scripts/mods/MarkPreview/MarkPreview_localization.lua
+++ MarkPreview/scripts/mods/MarkPreview/MarkPreview_localization.lua
@@ -2,9 +2,11 @@ return {
 	mod_name = {
 		en = "MarkPreview",
 		["zh-cn"] = "型号预览",
+		ru = "Предосмотр моделей",
 	},
 	mod_description = {
 		en = "See the difference between weapon marks in the mark menu.",
 		["zh-cn"] = "在型号菜单中查看不同武器型号之间的差别。",
+		ru = "MarkPreview - Cмотрите разницу между моделями оружия в меню выбора моделей.",
 	},
 }
