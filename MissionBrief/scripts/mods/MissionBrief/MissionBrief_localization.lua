024751cf1b01b8586de75cd3bf9d5ab23ff146e9
alxl <alex.cavasso@gmail.com>
2024-11-20 16:15:14 -0600

    MissionBrief - restore ru, zh-cn localization

--- MissionBrief/scripts/mods/MissionBrief/MissionBrief_localization.lua
+++ MissionBrief/scripts/mods/MissionBrief/MissionBrief_localization.lua
@@ -1,9 +1,13 @@
 return {
 	mod_name = {
 		en = "MissionBrief",
+		ru = "Брифинг миссии",
+		["zh-cn"] = "任务简报",
 	},
 	mod_description = {
 		en = "Shows mission details in the loading screen.",
+		ru = "MissionBrief - Показывает подробности миссии на экране загрузки.",
+		["zh-cn"] = "在加载界面显示任务详情。",
 	},
 	show_mission = {
 		en = Localize("loc_mission_voting_view_show_details")
