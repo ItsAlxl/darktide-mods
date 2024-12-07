3c9441c4a03226f630c95e581f7a3a07b2da3ecf
alxl <alex.cavasso@gmail.com>
2024-12-07 11:21:02 -0600

    SortBrunt works the first time in craft view

--- SortBrunt/scripts/mods/SortBrunt/SortBrunt.lua
+++ SortBrunt/scripts/mods/SortBrunt/SortBrunt.lua
@@ -16,6 +16,7 @@ mod:hook_safe(CLASS.MasteriesOverviewView, "_setup_layout_entries", function(sel
 	table.sort(self._masteries_layout, _sort_masteries)
 end)
 
-mod:hook_safe(CLASS.CraftingMechanicusBarterItemsView, "_setup_menu_tabs", function(self)
+mod:hook(CLASS.CraftingMechanicusBarterItemsView, "_setup_menu_tabs", function(func, self, ...)
 	table.sort(self._patterns_layout, _sort_masteries)
+	func(self, ...)
 end)
