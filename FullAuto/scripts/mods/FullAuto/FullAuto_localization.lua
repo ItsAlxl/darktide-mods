723c3deac2cca842672848bd55c5a10009d1ac0d
SyuanTsai <carsun00@Gmail.com>
2025-07-23 17:31:30 +0800

    Add zh-tw

--- FullAuto/scripts/mods/FullAuto/FullAuto_localization.lua
+++ FullAuto/scripts/mods/FullAuto/FullAuto_localization.lua
@@ -3,64 +3,78 @@ return {
 		en = "FullAuto",
 		["zh-cn"] = "全自动开火",
 		ru = "Полный автомат",
+		["zh-tw"] = "全自動開火",
 	},
 	mod_description = {
 		en = "Make all possible ranged weapons fully automatic.",
 		["zh-cn"] = "修改所有远程武器为全自动模式。",
 		ru = "FullAuto - Сделайте всё возможное оружие дальнего боя полностью автоматическим.",
+		["zh-tw"] = "修改遠程武器變為全自動。",
 	},
 	default_autofire = {
 		en = "Default to Automatic Firemode",
 		["zh-cn"] = "默认为自动开火模式",
 		ru = "Переключение режимов стрельбы",
+		["zh-tw"] = "預設為自動射擊模式",
 	},
 	pressed_autoshoot = {
 		en = "Automatic Firemode (Toggle)",
 		["zh-cn"] = "自动开火模式（切换）",
 		ru = "Автоматический режим огня (переключить)",
+		["zh-tw"] = "自動開火模式（切換）",
 	},
 	held_autoshoot = {
 		en = "Automatic Firemode (Held)",
 		["zh-cn"] = "自动开火模式（按住）",
 		ru = "Автоматический режим огня (удерживать)",
+		["zh-tw"] = "自動開火模式（長按）",
 	},
 	shoot_for_me = {
 		en = "Constantly shoot when on Automatic Firemode",
 		["zh-cn"] = "自动开火模式下始终开火",
 		ru = "Постоянно стрелять в автоматическом режиме",
+		["zh-tw"] = "自動開火模式下持續開火",
 	},
 	remember_per_wep = {
 		en = "Remember firemode per-weapon",
 		["zh-cn"] = "分武器记住开火模式",
 		ru = "Запоминать режим огня для каждого оружия.",
+		["zh-tw"] = "分別記住各武器開火模式",
 	},
 	include_psyker_bees = {
-		en = Localize("loc_class_psyker_title") .. " " .. Localize("loc_ability_psyker_blitz_throwing_knives")
+		en = Localize("loc_class_psyker_title") .. " " .. Localize("loc_ability_psyker_blitz_throwing_knives"),
+		["zh-tw"] = Localize("loc_class_psyker_title") .. " " .. Localize("loc_ability_psyker_blitz_throwing_knives"),
 	},
 	chargeup_autofire = {
 		en = Localize("loc_weapon_keyword_charged_attack"),
+		["zh-tw"] = Localize("loc_weapon_keyword_charged_attack"),
 	},
 	chargeup_autofire_amt = {
 		en = Localize("loc_weapon_keyword_charged_attack") .. " %%",
+		["zh-tw"] = Localize("loc_weapon_keyword_charged_attack") .. " %%",
 	},
 	hud_element = {
 		en = "HUD Indicator",
 		["zh-cn"] = "HUD 指示器",
 		ru = "Индикатор в интерфейсе",
+		["zh-tw"] = "HUD 指示器",
 	},
 	hud_element_size = {
 		en = "Indicator Size",
 		["zh-cn"] = "指示器大小",
 		ru = "Размер индикатора",
+		["zh-tw"] = "指示器大小",
 	},
 	group_select = {
 		en = "Firemode Selection",
 		["zh-cn"] = "开火模式选择",
 		ru = "Выбор режима огня",
+		["zh-tw"] = "開火模式選擇",
 	},
 	group_extra = {
 		en = "Misc",
 		["zh-cn"] = "杂项",
 		ru = "Разное",
+		["zh-tw"] = "雜項",
 	},
 }
--- LoadoutNames/scripts/mods/LoadoutNames/LoadoutNames_localization.lua
+++ LoadoutNames/scripts/mods/LoadoutNames/LoadoutNames_localization.lua
@@ -3,15 +3,18 @@ return {
 		en = "LoadoutNames",
 		["zh-cn"] = "配装命名",
 		ru = "Названия билдов",
+		["zh-tw"] = "裝備命名",
 	},
 	mod_description = {
 		en = "Name your loadouts.",
 		["zh-cn"] = "给你的配装命名。",
 		ru = "Loadout Names — Дайте названия вашим вариантам билдов.",
+		["zh-tw"] = "給你的裝備命名。",
 	},
 	name_placeholder = {
 		en = "Loadout Name",
 		["zh-cn"] = "配装名称",
 		ru = "Название билда",
+		["zh-tw"] = "裝備名稱",
 	},
 }
