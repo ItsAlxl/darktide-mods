9718272c411901d9a68119e207545f406a04f903
alxl <its.alxl@proton.me>
2025-09-23 16:36:01 -0500

    NoNews - Bound by Duty

--- NoNews/scripts/mods/NoNews/NoNews.lua
+++ NoNews/scripts/mods/NoNews/NoNews.lua
@@ -1,5 +1,5 @@
 local mod = get_mod("NoNews")
 
-mod:hook_safe(CLASS.MainMenuView, "on_enter", function(self)
-    self._news_element._visible = false
+mod:hook(CLASS.ViewElementNewsSlide, "_initialize_slides", function(self, ...)
+	self:set_visibility(false)
 end)
