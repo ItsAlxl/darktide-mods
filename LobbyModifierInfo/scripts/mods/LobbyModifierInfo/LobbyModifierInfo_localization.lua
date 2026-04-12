f1367b4cb5c92b183087eef0e637a2bdca80d6dc
xsSplater - СанСаныч <xssplater@gmail.com>
2026-04-12 20:03:49 +0300

    Update RU LobbyModifierInfo_localization.lua

--- LobbyModifierInfo/scripts/mods/LobbyModifierInfo/LobbyModifierInfo_localization.lua
+++ LobbyModifierInfo/scripts/mods/LobbyModifierInfo/LobbyModifierInfo_localization.lua
@@ -1,8 +1,10 @@
 return {
 	mod_name = {
-		en = "LobbyModifierInfo",
+		en = "Lobby Modifier Info",
+		ru = "Инфо о модификаторах в лобби",
 	},
 	mod_description = {
 		en = "Displays mission modifier information in the lobby screen",
+		ru = "Lobby Modifier Info - Показывает информацию о модификаторах миссии на экране лобби",
 	},
 }
