983d773ed0a89a926aa8ccc633fd4a647addaf8d
alxl <its.alxl@proton.me>
2026-07-08 05:56:19 -0500

    NoNews fix harmless error

--- NoNews/scripts/mods/NoNews/NoNews.lua
+++ NoNews/scripts/mods/NoNews/NoNews.lua
@@ -1,5 +1,1 @@
-local mod = get_mod("NoNews")
-
-mod:hook(CLASS.ViewElementNewsSlide, "_initialize_slides", function(self, ...)
-	self:set_visibility(false)
-end)
+get_mod("NoNews"):hook(CLASS.ViewElementNewsSlide, "_initialize_slides", function() end)
